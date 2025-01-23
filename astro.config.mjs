13c8be1bb7b50e149507dad1cec8636b065d050c
Tom Avenel <tom@avenel.pro>
2025-01-23 23:50:23 +0100

    astro: default markdown layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Set default Layout for Markdown files
+const setLayout = () => {
+	return function (_, file) {
+		file.data.astro.frontmatter.layout =
+			file.data.astro.frontmatter.layout || "@layouts/CourseLayout.astro";
+	};
+};
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	markdown: {
+		remarkPlugins: [setLayout],
+	},
+})
