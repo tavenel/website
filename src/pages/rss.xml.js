cbcdcdbf8521a3c7b1dc145bdcea6dba116ce0b4
Tom Avenel <tom@avenel.pro>
2025-01-27 21:52:08 +0100

    rss: WIP

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,24 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
-import sanitizeHtml from 'sanitize-html';
+// import sanitizeHtml from 'sanitize-html';
 
-export function GET(context) {
-	const postImportResult = pagesGlobToRssItems(import.meta.glob('./**/*.md', { eager: true }));
-	const posts = Object.values(postImportResult);
+export async function GET(context) {
+	// const postImportResult = pagesGlobToRssItems(import.meta.glob('./**/*.md', { eager: true }));
+	// const posts = Object.values(postImportResult);
 	return rss({
 		title: 'Tom Avenel',
 		description: 'Tech update and courses',
 		site: context.site,
+		// Array of `<item>`s in output xml
 		// See "Generating items" section for examples using content collections and glob imports
-		items: posts.map(async (post) => ({
-			link: post.url,
-			content: sanitizeHtml((await post.compiledContent())),
-			...post.frontmatter,
-		})),
-		stylesheet: '/rss/pretty-feed-v3.xsl',
+		// items: posts.map(async (post) => ({
+		// 	link: post.url,
+		// 	content: sanitizeHtml((await post.compiledContent())),
+		// 	...post.frontmatter,
+		// })),
+		items: await pagesGlobToRssItems(
+			import.meta.glob('./**/*.{md,mdx}'),
+		),
+		stylesheet: '/rss/styles.xsl',
 	});
 }
 
