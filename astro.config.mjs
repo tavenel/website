7651729774884ee2e50dc0083a85a957e30dba6a
Tom Avenel <tom@avenel.pro>
2025-01-27 13:08:15 +0100

    astro: remark-toc and rehype-accessible-emojis

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import remarkToc from 'remark-toc';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 // Set default Layout for Markdown files
 const setLayout = () => {
@@ -16,7 +18,8 @@ export default defineConfig({
 	site: 'https://www.avenel.pro',
 
 	markdown: {
-		remarkPlugins: [setLayout],
+		remarkPlugins: [setLayout, [remarkToc, { heading: 'Chapitres', maxDepth: 3 }]],
+		rehypePlugins: [rehypeAccessibleEmojis],
 		shikiConfig: {
 			// https://shiki.style/themes
 			themes: {
