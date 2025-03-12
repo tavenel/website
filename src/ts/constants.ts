ed726de29fc87dc6e7f290a63b8dcbd430fe4d2a
Tom Avenel <tom@avenel.pro>
2025-03-12 19:22:24 +0100

    astro: github and srht links + fix astro paths

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,1 +1,4 @@
-export const GIT_FILE_TREE_PAGES = "https://git.sr.ht/~toma/astro/tree/main/item/src/pages";
+// SourceHut files path.
+export const SRHT_FILE_TREE_PAGES = "https://git.sr.ht/~toma/astro/tree/main/item/src/pages";
+// Github files path
+export const GITHUB_FILE_TREE_PAGES = "https://github.com/tavenel/website/tree/main/src/pages";
