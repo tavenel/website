5b2bce989e70815fe89850deb9d1f6f419fe7fa9
Tom Avenel <tom@avenel.pro>
2025-08-01 10:50:32 +0200

    cleanup -- rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,6 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 // import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
-	// const postImportResult = pagesGlobToRssItems(import.meta.glob('./**/*.md', { eager: true }));
-	// const posts = Object.values(postImportResult);
 	return rss({
 		title: 'Tom Avenel',
 		description: 'Tech update and courses',
