0c093bf0ad10301630863fe91cbc1b3b9de8f0ea
Tom Avenel <tom@avenel.pro>
2025-08-01 23:07:25 +0200

    playwright -- conf -- URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'https://avenel.pro',
+		baseURL: process.env.DEPLOYMENT_URL ?? 'https://avenel.pro',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
